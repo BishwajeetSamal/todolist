41a09abb32e5dc31b8f02da0adc1b459331c9be2
Bishwajeet Samal <bishwajeetsamal10@gmail.com>
2020-12-29 01:35:48 +0530

    code get short

--- date.js
+++ date.js
@@ -1,15 +1,16 @@
 //jshint esversion:6
-module.exports=getDate;
-function getDate(){
+
+//over here we can use "module.exports" or only  "exports.function name"
+ exports.getDate = function getDate(){ 
 
 
-    let today = new Date();
-    let options = {
+    const today = new Date();
+    const options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
-    let day = today.toLocaleDateString("en-US", options);
-    return day;
+    return  today.toLocaleDateString("en-US", options);
+    
 
 }
